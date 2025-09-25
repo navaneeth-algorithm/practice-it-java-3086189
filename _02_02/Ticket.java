fcd6dddd70e75f925341de14649bfaca2639c1a6
Navaneeth B <30873536+navaneeth-algorithm@users.noreply.github.com>
2025-09-25 23:25:44 +0000

    completed

---
+++ _02_02/Ticket.java
@@ -0,0 +1,12 @@
+package _02_02;
+
+public class Ticket {
+  //3 private variables
+  private String destination;
+  private double price;
+  private boolean isReturn;
+
+  //Empty Constructor
+  public Ticket() {
+  }
+}
